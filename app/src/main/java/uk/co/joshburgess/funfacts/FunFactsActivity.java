cda6fae5e4691b9351bf442214d7ecf8ef3198c7
woJburgess <jburgess@whiteoctober.co.uk>
2016-06-24 14:01:24 +0100

    Adding the onClick() Method

--- app/src/main/java/uk/co/joshburgess/funfacts/FunFactsActivity.java
+++ app/src/main/java/uk/co/joshburgess/funfacts/FunFactsActivity.java
@@ -1,7 +1,11 @@
 package uk.co.joshburgess.funfacts;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class FunFactsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        final TextView factLabel = (TextView) findViewById(R.id.funFactTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = "A fact goes here";
+
+                factLabel.setText(fact);
+            }
+        };
+
+        showFactButton.setOnClickListener(listener);
     }
 }
